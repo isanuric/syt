f35549b5bd4b90ea0602a2c76dd06dde77613228
isanuric <isanuric@gmail.com>
2016-05-15 20:06:04 +0200

    commit src

---
+++ src/header.h
@@ -0,0 +1,8 @@
+/*
+ * header.h
+ *
+ *  Created on: May 15, 2016
+ *      Author: ehsan
+ */
+
+
---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: May 15, 2016
+ *      Author: ehsan salmani
+ */
+
+
+#include "header.h"
+
+
+int main() {
+
+
+	return 0;
+}
